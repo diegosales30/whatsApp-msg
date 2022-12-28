8a1075c3f42309941264d8f11e3770a40a1d208d
diegosales30 <diegofelipesales23@gmail.com>
2022-12-28 15:01:38 -0300

    gerenciamento de estado com redux toolkit

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -8,6 +8,7 @@ import { reducer } from "../utils/reducers/formReducer";
 import { signUp } from "./../utils/actions/authActions";
 import { ActivityIndicator, Alert } from "react-native";
 import colors from "../constants/colors";
+import { useDispatch, useSelector } from "react-redux";
 
 const initialState = {
   inputValues: {
@@ -26,6 +27,10 @@ const initialState = {
 };
 
 const SignUpForm = (props) => {
+  const dispatch = useDispatch();
+  // const userData = useSelector((state) => state.auth);
+  // console.log(stateData);
+
   const [error, setError] = useState();
   const [isLoading, setIsLoanding] = useState(false);
   const [formState, dispatchFormState] = useReducer(reducer, initialState);
@@ -47,12 +52,15 @@ const SignUpForm = (props) => {
   const authHandler = async () => {
     try {
       setIsLoanding(true);
-      await signUp(
+
+      const action = signUp(
         formState.inputValues.firstName,
         formState.inputValues.lastName,
         formState.inputValues.email,
         formState.inputValues.password
       );
+      dispatch(action);
+
       setError(null);
     } catch (error) {
       setError(error.message);
